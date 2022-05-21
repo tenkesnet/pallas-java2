6ff54151f856e0ad07b0963e8bb21a03dbcdcc4b
Bardonicsek Robert <rbardo@gmail.com>
2022-05-21 14:43:19 +0200

    Exceptions kezelés

--- pelda1.java
+++ pelda1.java
@@ -16,36 +16,68 @@ public class pelda1 {
         int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         String[] b = new String[3];
         String[] c = new String[3];
-        b[0] = "banán";
-        b[1] = "körte";
-        //b[2] = "alma";
 
-        /* ArrayList<String> nevek = new ArrayList<String>(Arrays.asList("Őrs","Robi","Géza","István","Robi"));
-        
-
-        ArrayList<String> robik = new ArrayList<String>(Arrays.asList("Robi"));
-        
-        //nevek.removeAll(robik);
-        
-        nevek.sort(String::compareToIgnoreCase);
-
-        for( String nev : nevek){
-            System.out.println(nev);
-        } */
-
-        ArrayList<Kor> korok = new ArrayList<Kor>();
-        korok.add( new Kor(4));
-        korok.add( new Kor(6));
-        korok.add( new Kor(13));
-
-        System.out.println( new Kor(13).equals("13-askor") );
-
-        korok.remove(new Kor(13));
-
-        //System.out.println(new Kor(45).equals(new Kor(46)));
-        for( Kor k : korok){
-            System.out.println(k.r()+" sugarú körnek a területe("+k.hashCode()+"): "+ k.terulet());
+        try {
+            b[0] = "banán";
+            b[1] = "körte";
+            b[4] = "füge";
+        } catch (Exception e) {
+            System.err.println("A tömb túl lett címezve " + e.toString() + "\n\n");
+        } finally {
+            for (String item : b) {
+                System.out.println(item);
+            }
         }
+
+        // b[2] = "alma";
+
+        /*
+         * ArrayList<String> nevek = new
+         * ArrayList<String>(Arrays.asList("Őrs","Robi","Géza","István","Robi"));
+         * 
+         * 
+         * ArrayList<String> robik = new ArrayList<String>(Arrays.asList("Robi"));
+         * 
+         * //nevek.removeAll(robik);
+         * 
+         * nevek.sort(String::compareToIgnoreCase);
+         * 
+         * for( String nev : nevek){
+         * System.out.println(nev);
+         * }
+         */
+        ArrayList<Kor> korok = new ArrayList<Kor>();
+        korok.add(new Kor(4));
+        korok.add(new Kor(6));
+        korok.add(new Kor(13));
+        try {
+
+            System.out.println(new Kor(13).equals("13-askor"));
+
+            korok.remove(new Kor(13));
+
+            // System.out.println(new Kor(45).equals(new Kor(46)));
+            for (Kor k : korok) {
+                System.out.println(k.r() + " sugarú körnek a területe(" + k.hashCode() + "): " + k.terulet());
+            }
+        } catch (ClassCastException ec) {
+            System.err.println("Az összehasonlítás nem lehetséges");
+        } catch (Exception e) {
+            if (e instanceof ClassCastException) {
+                System.err.println("Konverziós hiba");
+            } else {
+                System.err.println(e.getMessage());
+            }
+        }
+
+        finally {
+            korok.remove(new Kor(13));
+            for (Kor k : korok) {
+                System.out.println(k.r() + " sugarú körnek a területe(" + k.hashCode() + "): " + k.terulet());
+            }
+        }
+
+        System.out.println("Program normálisan befejeződött");
 
     }
 }
