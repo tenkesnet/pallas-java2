cea95b5e1f19ca82c480f95599fad4acb1baec5b
tenkesnet <rbardo@gmail.com>
2022-06-18 08:36:20 +0200

    2022-06-18 init

---
+++ pelda2.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class pelda2 {
+    public static void main(String[] args) {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create("https://www.google.com"))
+                    .build();
+            HttpResponse<String> response = httpClient.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Status code: " + response.statusCode());
+            System.out.println("Headers: " + response.headers().allValues("content-type"));
+            System.out.println("Body: " + response.body());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
